f519f2e1e347b5b73d6699bfa2e55e1dc0dc7404
Thanh Phong <ttphong2608@gmail.com>
2018-06-24 15:35:34 +0700

    Initial Commit

---
+++ TwitSplit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwitSplit
+//
+//  Created by ThanhPhong-Tran on 6/24/18.
+//  Copyright © 2018 ECDC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
