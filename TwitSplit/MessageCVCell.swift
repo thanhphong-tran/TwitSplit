a398fc293d7ea3b96fcf5b6825bfe5c21e8dc029
Thanh Phong <ttphong2608@gmail.com>
2018-06-27 00:28:31 +0700

    [add] message cell

---
+++ TwitSplit/MessageCVCell.swift
@@ -0,0 +1,21 @@
+//
+//  MessageCVCell.swift
+//  TwitSplit
+//
+//  Created by ThanhPhong-Tran on 6/26/18.
+//  Copyright © 2018 ECDC. All rights reserved.
+//
+
+import UIKit
+
+class MessageCVCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var lbMessage: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- TwitSplit/ViewController.swift
+++ TwitSplit/ViewController.swift
@@ -8,18 +8,64 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+extension String {
+    func width(withConstraintedHeight height: CGFloat, font: UIFont) -> CGFloat {
+        let constraintRect = CGSize(width: .greatestFiniteMagnitude, height: height)
+        let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSAttributedStringKey.font: font], context: nil)
+        
+        return boundingBox.width
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func height(withConstraintedWidth width: CGFloat, font: UIFont) -> CGFloat {
+        let constraintRect = CGSize(width: width, height: .greatestFiniteMagnitude)
+        let boundingBox = self.boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [NSAttributedStringKey.font: font], context: nil)
+        
+        return boundingBox.height
     }
-
-
 }
 
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tfMessage: UITextField!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var dataSource: [String] = [
+        "abababababbabababbababababbababababba",
+        "abababababbabababbababababbababababba",
+        "abababababbabababbababababbababababba",
+        "a"
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.collectionView.dataSource = self
+        self.collectionView.delegate = self
+        self.collectionView.register(UINib.init(nibName: "MessageCVCell", bundle: nil), forCellWithReuseIdentifier: "MessageCVCell")
+    }
+    
+    @IBAction func sendPressed(_ sender: Any) {
+        
+    }
+    
+}
+
+
+extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return dataSource.count
+    }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MessageCVCell", for: indexPath) as! MessageCVCell
+        cell.lbMessage.text = dataSource[indexPath.row]
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width: CGFloat = collectionView.frame.width
+        let height: CGFloat = dataSource[indexPath.row].height(withConstraintedWidth: 250.0, font: UIFont.systemFont(ofSize: 17.0))
+        return CGSize.init(width: width, height: height + 20)
+    }
+}
